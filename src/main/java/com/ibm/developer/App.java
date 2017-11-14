334575ea4a09be737dda655bb4ff62a90bdbc9d9
Tokida Hideaki <tokihide@gmail.com>
2017-11-14 16:19:01 +0900

    fix bluemix

--- src/main/java/com/ibm/developer/App.java
+++ src/main/java/com/ibm/developer/App.java
@@ -10,14 +10,10 @@ import java.net.Socket;
  */ 
 public class App { 
     public static void main(String[] args) throws IOException { 
-        int port = Integer.parseInt(System.getenv("VCAP_APP_PORT")); 
-        ServerSocket ss = new ServerSocket(port); 
+	int port = Integer.parseInt(System.getenv("VCAP_APP_PORT")); ServerSocket ss = new ServerSocket(port); 
         while (true) { 
             Socket s = ss.accept(); 
-            s.getOutputStream() 
-                    .write(("HTTP/1.1 200 OK\nContent-Type:
-text/html; charset=utf-8\nContent-Length: 2\n\n" +
-veryComplicatedMethod()) .getBytes()); 
+            s.getOutputStream().write(("HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\nContent-Length: 2\n\n" + veryComplicatedMethod()) .getBytes()); 
         } 
     } 
  
