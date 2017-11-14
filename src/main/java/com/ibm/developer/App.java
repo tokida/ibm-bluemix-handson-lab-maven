bd0cf233633cab34bd35a647b2a1c7a43a3d83e7
Tokida Hideaki <tokihide@gmail.com>
2017-11-14 16:11:02 +0900

    fix bluemix

--- src/main/java/com/ibm/developer/App.java
+++ src/main/java/com/ibm/developer/App.java
@@ -1,13 +1,28 @@
-package com.ibm.developer;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+package com.ibm.developer; 
+ 
+import java.io.IOException; 
+import java.net.ServerSocket; 
+import java.net.Socket; 
+ 
+/** 
+* Hello world! 
+ * 
+ */ 
+public class App { 
+    public static void main(String[] args) throws IOException { 
+        int port = Integer.parseInt(System.getenv("VCAP_APP_PORT")); 
+        ServerSocket ss = new ServerSocket(port); 
+        while (true) { 
+            Socket s = ss.accept(); 
+            s.getOutputStream() 
+                    .write(("HTTP/1.1 200 OK\nContent-Type:
+text/html; charset=utf-8\nContent-Length: 2\n\n" +
+veryComplicatedMethod()) .getBytes()); 
+        } 
+    } 
+ 
+    public static int veryComplicatedMethod() { 
+        return 42; 
+    } 
 }
+
